1852237999af97000dd9d7af7c5677c535d9c5cb
Gaston Aganza <gaston.aganza@gmail.com>
2020-06-11 19:17:05 -0700

    init commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const auth = require("./auth.json") || { token: process.env.TOKEN };
+
+client.login(auth.token);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/bot-info", (req, res, next) => {
+  let data = client.toJSON();
+
+  let guilds = data["guilds"];
+  let channels = data["channels"];
+  let users = data["users"];
+
+  res.send({
+    guilds_count: guilds.length,
+    channels_count: channels.length,
+    users_count: users.length,
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
